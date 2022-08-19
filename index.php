711dc5b12411c75d3a6a6687d51fc9c48d504c0b
alaa140101 <alaa140011@gmail.com>
2022-08-19 23:36:10 +0300

    theme starter

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+This is test content
+
+
+<i class="bi-alarm text-dark" style="font-size: 2rem; color: cornflowerblue;"></i>
+
+
+
+<i class="fas fa-camera-retro"></i>
+
+
+<?php get_footer(); ?>
