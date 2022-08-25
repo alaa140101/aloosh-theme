c618fe47d218def8a5bb8b3093fabe054cc49b29
alaa140101 <alaa140011@gamil.com>
2022-08-25 14:58:17 +0300

    01:34

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function aloosh_theme_support()
 {
   // Adds dynamic title tag support
   add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme', 'aloosh_theme_support');
